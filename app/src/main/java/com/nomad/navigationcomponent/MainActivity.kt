102d9685e158286ec4e913a774502f1ebb853f35
Ekmadish <estaykargabay1206@gmail.com>
2021-03-01 14:29:06 +0600

    Navigation Drawer

--- app/src/main/java/com/nomad/navigationcomponent/MainActivity.kt
+++ app/src/main/java/com/nomad/navigationcomponent/MainActivity.kt
@@ -9,21 +9,26 @@ import androidx.navigation.NavGraph
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.onNavDestinationSelected
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController:NavController
+    private  lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navHostFragment=supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController=navHostFragment.findNavController()
+        appBarConfiguration= AppBarConfiguration(
+                setOf(R.id.homeFragment,R.id.searchFragment),
+                draw_layout
+        )
         setSupportActionBar(toolbar)
-        setupActionBarWithNavController(navController)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        bottom_nav.setupWithNavController(navController)
+        nav_view.setupWithNavController(navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -43,6 +48,6 @@ class MainActivity : AppCompatActivity() {
 
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration ) || super.onSupportNavigateUp()
     }
 }
